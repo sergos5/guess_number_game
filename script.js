bedf0fc794c415472fdadee665dc5fa5239de1d4
Sergey <sergos5@yandex.ru>
2024-03-04 20:27:22 +0300

    угадай число дифф

--- script.js
+++ script.js
@@ -3,30 +3,48 @@
 //Загадывание случайного числа
 
 let guess = 'Угадай число от 1 до 100';
-let less = 'Загаданное число меньше';
-let more = 'Загаданное число больше';
+let less = 'Загаданное число меньше, осталось попыток - ';
+let more = 'Загаданное число больше, осталось попыток - ';
 let end = 'Игра окончена';
-let congratulation = 'Поздравляю, Вы угадали!!!'; 
+let congratulation = 'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?'; 
+let endAtt = 'Попытки закончились, хотите сыграть еще?';
 let err = 'Введи число!';
 
-let guessNumber = function(data) {    
-    let foo = function(message) {
-        let num = prompt(message);       
-        if (isNaN(num)) {
-            foo(err);
-        } else if (num === null) {
-            alert (end);
-        } else if (num<data) {
-            foo(more); 
-        } else if (num>data) {
-            foo(less);
-        } else {
-            alert(congratulation);
-        }
+
+let randNum = function () {
+    return Math.round(Math.random()*100);
+};
+
+let guessNumber = function(data) {  
+    let n = 10;    
+    let foo = function(message) {   
+        n--;    
+        if (n>=0) {
+            let num = prompt(message);             
+            if (isNaN(num)) {
+                foo(err);
+            } else if (num === null) {
+                alert (end);
+            } else if (num<data) {
+                foo(more + n); 
+            } else if (num>data) {
+                foo(less + n);
+            } else {
+                let a = confirm(congratulation);
+                if (a) {
+                    guessNumber(randNum());
+                }
+            }
+        } else  {
+            let a = confirm(endAtt);
+            if (a) {
+                guessNumber(randNum());
+            }
+        }      
     };
     foo(guess);
 };
 
-guessNumber(61);
+guessNumber(randNum());
 
 
